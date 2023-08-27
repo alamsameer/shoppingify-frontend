b76ca3356b2bce3587427a07fdfa3bc3983c1949
Sameer Alam <alamsameer738@gmail.com>
2023-08-27 22:59:16 +0530

    addedd bacjend url

--- src/store/apiservice.js
+++ src/store/apiservice.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api', // Set your API base URL here
+  baseURL: 'https://shoppingify-backend-three.vercel.app/api', // Set your API base URL here
 });
 
 // for items
